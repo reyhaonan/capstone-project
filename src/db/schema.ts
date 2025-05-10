3d72b3ddd6fd471d5d9a713d1caba8cbd843c0c0
reyhaonan <reyhaonan2003@gmail.com>
2025-05-10 22:02:33 +0700

    db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,80 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
+import {
+	boolean,
+	date,
+	pgEnum,
+	pgTable,
+	primaryKey,
+	serial,
+	text,
+	timestamp,
+	uuid,
+	varchar,
+} from 'drizzle-orm/pg-core'
 
-export const usersTable = pgTable('users', {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-	name: varchar({ length: 255 }).notNull(),
-	age: integer().notNull(),
-	email: varchar({ length: 255 }).notNull().unique(),
+export const users = pgTable('users', {
+	userId: uuid('user_id').primaryKey().defaultRandom(),
+	firstName: varchar('first_name'),
+	lastName: varchar('last_name'),
+	email: varchar('email').notNull(),
+	password: varchar('password').notNull(),
+	phoneNumber: varchar('phone_number'),
+	dateOfBirth: date('date_of_birth'),
+	address: text('address'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at').defaultNow(),
+})
+
+export const doctors = pgTable('doctors', {
+	doctorId: uuid('doctor_id').primaryKey().defaultRandom(),
+	firstName: varchar('first_name'),
+	lastName: varchar('last_name'),
+	specialization: varchar('specialization'),
+	licenseNumber: varchar('license_number'),
+	email: varchar('email').notNull(),
+	phoneNumber: varchar('phone_number'),
+	hospitalAffiliation: varchar('hospital_affiliation'),
+	password: varchar('password').notNull(),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at').defaultNow(),
+})
+
+export const statusEnum = pgEnum('status', ['ONGOING', 'CLOSED'])
+
+export const usersDoctors = pgTable(
+	'users_doctors',
+	{
+		userId: uuid('user_id').notNull(),
+		doctorId: uuid('doctor_id').notNull(),
+		createdAt: timestamp('created_at').defaultNow(),
+		status: statusEnum('status').notNull().default('ONGOING'),
+	},
+	(table) => [primaryKey({ columns: [table.userId, table.doctorId] })]
+)
+export const chats = pgTable('chats', {
+	chatId: serial('chat_id').primaryKey(),
+	userId: uuid('user_id')
+		.notNull()
+		.references(() => users.userId),
+	doctorId: uuid('doctor_id')
+		.notNull()
+		.references(() => doctors.doctorId),
+	messageText: text('message_text'),
+	isFromDoctor: boolean('is_from_doctor'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at').defaultNow(),
+})
+
+export const doctorReferrals = pgTable('doctor_referrals', {
+	referralId: uuid('referral_id').primaryKey().defaultRandom(),
+	userId: uuid('user_id')
+		.notNull()
+		.references(() => users.userId),
+	doctorId: uuid('doctor_id')
+		.notNull()
+		.references(() => doctors.doctorId),
+	referralReason: text('referral_reason'),
+	referralDate: timestamp('referral_date'),
+	notes: text('notes'),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at').defaultNow(),
 })
