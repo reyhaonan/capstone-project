7fc1eef44169fd04bab8a2a43cc3a482f4f164ab
reyhaonan <reyhaonan2003@gmail.com>
2025-05-12 23:08:13 +0700

    feat: reduce access token expiration time to 5 minutes and add JWT_SECRET to .env.example

--- src/db/model.ts
+++ src/db/model.ts
@@ -1,7 +1,31 @@
 import { table } from './schema'
 import { spreads } from './utils'
+import { createInsertSchema, createSelectSchema } from 'drizzle-typebox'
+import { t } from 'elysia'
 
 export const dbModel = {
-	insert: spreads(table, 'insert'),
-	select: spreads(table, 'select'),
+	insert: spreads(
+		{
+			...table,
+			users: createInsertSchema(table.users, {
+				email: t.String({ format: 'email' }),
+			}),
+			doctors: createInsertSchema(table.doctors, {
+				email: t.String({ format: 'email' }),
+			}),
+		},
+		'insert'
+	),
+	select: spreads(
+		{
+			...table,
+			users: createSelectSchema(table.users, {
+				email: t.String({ format: 'email' }),
+			}),
+			doctors: createSelectSchema(table.doctors, {
+				email: t.String({ format: 'email' }),
+			}),
+		},
+		'select'
+	),
 } as const
