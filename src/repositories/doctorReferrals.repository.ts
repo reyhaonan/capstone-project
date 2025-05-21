317c28ad9cc920d9f10576f3367e4332c60762e9
reyhaonan <reyhaonan2003@gmail.com>
2025-05-21 22:56:43 +0700

    feat: enhance doctor referrals retrieval to include user and doctor names

--- src/repositories/doctorReferrals.repository.ts
+++ src/repositories/doctorReferrals.repository.ts
@@ -28,13 +28,28 @@ export const getDoctorReferralById = async (referralId: string) => {
 
 export const getDoctorReferralsByUserOrDoctorId = async (id: string) => {
 	return db
-		.select()
+		.select({
+			referralId: table.doctorReferrals.referralId,
+			referralDate: table.doctorReferrals.referralDate,
+			referralReason: table.doctorReferrals.referralReason,
+			notes: table.doctorReferrals.notes,
+			createdAt: table.doctorReferrals.createdAt,
+			doctorId: table.doctorReferrals.doctorId,
+			userId: table.doctorReferrals.userId,
+			doctorName: table.doctors.name,
+			userName: table.users.name,
+		})
 		.from(table.doctorReferrals)
 		.where(
 			or(
 				eq(table.doctorReferrals.userId, id),
 				eq(table.doctorReferrals.doctorId, id)
 			)
+		)
+		.leftJoin(table.users, eq(table.users.userId, table.doctorReferrals.userId))
+		.leftJoin(
+			table.doctors,
+			eq(table.doctors.doctorId, table.doctorReferrals.doctorId)
 		)
 		.orderBy(desc(table.doctorReferrals.createdAt))
 }
