9bf92504092c4101d77c597463fde65a21df1938
reyhaonan <reyhaonan2003@gmail.com>
2025-05-21 23:03:08 +0700

    feat: remove create doctor referral endpoint and simplify referral retrieval

--- src/routes/doctorReferral.route.ts
+++ src/routes/doctorReferral.route.ts
@@ -1,11 +1,8 @@
 import { Elysia, t } from 'elysia'
 import {
-	createDoctorReferral,
 	getDoctorReferralById,
 	getDoctorReferralsByUserOrDoctorId,
 } from '@/repositories/doctorReferrals.repository'
-import { doctorAuthPlugin } from '@/plugins/doctorAuthPlugin'
-import { createDoctorReferralSchema } from '@/types/schema/doctorReferral.schema'
 import { agnosticAuthPlugin } from '@/plugins/agnosticAuthPlugin'
 
 export const referralRoutes = new Elysia({
@@ -17,7 +14,7 @@ export const referralRoutes = new Elysia({
 	// Doctor or user can access this route
 	.group('', (app) =>
 		app.use(agnosticAuthPlugin).get(
-			'/referrals',
+			'',
 			async ({ params: { referralId }, id }) => {
 				const referrals = await getDoctorReferralsByUserOrDoctorId(id)
 
@@ -70,28 +67,6 @@ export const referralRoutes = new Elysia({
 				detail: {
 					description: 'Only relevant user or doctor can fetch the referral',
 				},
-			}
-		)
-	)
-
-	.group('', (app) =>
-		app.use(doctorAuthPlugin).post(
-			'/create',
-			async ({ body, doctor, status }) => {
-				const [result] = await createDoctorReferral({
-					...body,
-					doctorId: doctor.doctorId,
-				})
-
-				if (!result) status('Conflict', 'Referral already exists')
-
-				return {
-					message: 'Referral created successfully',
-					data: result,
-				}
-			},
-			{
-				body: t.Omit(createDoctorReferralSchema, ['doctorId']),
 			}
 		)
 	)
